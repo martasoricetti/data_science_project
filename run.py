a246c39ede18a0cf1bc0d75f1e74a49175511d16
Salvadana <92596602+Salvadana@users.noreply.github.com>
2024-02-09 13:03:39 +0100

    final

--- run.py
+++ run.py
@@ -18,16 +18,16 @@ from impl_graph import *
 rel_path = "relational.db"
 rel_dp = RelationalDataProcessor()
 rel_dp.setDbPath(rel_path)
-#rel_dp.uploadData("data/relational_publications.csv")
-#rel_dp.uploadData("data/relational_other_data.json")
+rel_dp.uploadData("data/relational_publications.csv")
+rel_dp.uploadData("data/relational_other_data.json")
 
 # Then, create the RDF triplestore (remember first to run the
 # Blazegraph instance) using the related source data
-grp_endpoint = " http://10.250.13.101:9999/blazegraph/sparql"
+grp_endpoint = "http://10.250.13.195:9999/blazegraph/sparql"
 grp_dp = TriplestoreDataProcessor()
 grp_dp.setEndpointUrl(grp_endpoint)
-#grp_dp.uploadData("data/graph_publications.csv")
-#grp_dp.uploadData("data/graph_other_data.json")
+grp_dp.uploadData("data/graph_publications.csv")
+grp_dp.uploadData("data/graph_other_data.json")
 
 # In the next passage, create the query processors for both
 # the databases, using the related classes
@@ -44,15 +44,4 @@ generic = GenericQueryProcessor()
 generic.addQueryProcessor(rel_qp)
 generic.addQueryProcessor(grp_qp)
 
-#result_q1 = grp_qp.getJournalArticlesInJournal('issn:0219-1377')
-#result_q1 = generic.getMostCitedPublication()
-result_q2 = generic.getPublicationsByAuthorName('aldo') #only 2 but in the graph they are 5
-#result_q1 = generic.getPublicationsByAuthorName('jesualdo')['id'] #no obj to concatenate
-print(result_q2)
 
-#a= grp_qp.getOrganization(crossref_id="crossref:2780").getName()
-#a= rel_qp.getPublicationsPublishedInYear('2017')'''
-
-
-#result_q1 = grp_qp.getPublicationsByAuthorName('aldo')['id'] 
-#pprint(result_q1)
